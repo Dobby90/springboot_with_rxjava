b549297a00c3c496d366522ba2efcf9b03fe7fd7
dobby90 <extremezerg@naver.com>
2022-02-05 21:34:39 +0900

    1st Example

--- src/main/java/com/study/rxjava/RxjavaApplication.java
+++ src/main/java/com/study/rxjava/RxjavaApplication.java
@@ -3,11 +3,21 @@ package com.study.rxjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.reactivex.Observable;
+
 @SpringBootApplication
 public class RxjavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RxjavaApplication.class, args);
+
+		/**
+		 * RxJava Example
+		 */
+		// 1
+		Observable.just("Hello", "World")
+		.subscribe(System.out::println); // 발행을 해야 메서드가 실행 됨.
+
 	}
 
 }
