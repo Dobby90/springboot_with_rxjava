c93e6a8f16ca92fc46ef9883ad29efe13ee2ae35
dobby90 <extremezerg@naver.com>
2022-02-09 17:56:11 +0900

    Example 2-1 Test

--- src/main/java/com/study/rxjava/RxjavaApplication.java
+++ src/main/java/com/study/rxjava/RxjavaApplication.java
@@ -18,6 +18,12 @@ public class RxjavaApplication {
 		Observable.just("Hello", "World")
 		.subscribe(System.out::println); // 발행을 해야 메서드가 실행 됨.
 
+		// 2-1
+		Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+		.filter(f -> f % 2 == 0)	// 짝수에 해당하는 데이터만 통지한다
+		.map(f -> f * 100)			// 데이터를 100배로 변환한다
+		.subscribe(f -> System.out.println("data=" + f));
+
 	}
 
 }
