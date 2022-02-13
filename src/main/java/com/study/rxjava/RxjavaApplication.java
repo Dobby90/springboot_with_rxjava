a219b476b59b4e2281b1b048ec78f22f0a44c62d
dobby90 <extremezerg@naver.com>
2022-02-14 08:56:25 +0900

    Example 2-2 Test

--- src/main/java/com/study/rxjava/RxjavaApplication.java
+++ src/main/java/com/study/rxjava/RxjavaApplication.java
@@ -3,6 +3,7 @@ package com.study.rxjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 
 @SpringBootApplication
@@ -22,7 +23,15 @@ public class RxjavaApplication {
 		Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 		.filter(f -> f % 2 == 0)	// 짝수에 해당하는 데이터만 통지한다
 		.map(f -> f * 100)			// 데이터를 100배로 변환한다
-		.subscribe(f -> System.out.println("data=" + f));
+		.subscribe(f -> System.out.println("2-1) data=" + f));
+
+		// 2-2
+		Flowable<Integer> flowable = Flowable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+		.filter(f -> f % 2 == 0)	// 짝수에 해당하는 데이터만 통지한다
+		.map(f -> f * 100);			// 데이터를 100배로 변환한다
+
+		// 구독하고 받은 데이터를 출력한다
+		flowable.subscribe(f -> System.out.println("2-2) data=" + f));
 
 	}
 
