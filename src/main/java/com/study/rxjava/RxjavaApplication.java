a0ff704f4900556b3ef06b3ec0c1641a4f2d7920
Dobby90 <extremezerg@naver.com>
2022-02-18 12:50:19 +0900

    Example 3-1,2 Test

--- src/main/java/com/study/rxjava/RxjavaApplication.java
+++ src/main/java/com/study/rxjava/RxjavaApplication.java
@@ -1,5 +1,9 @@
 package com.study.rxjava;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -38,6 +42,23 @@ public class RxjavaApplication {
 		.filter(f -> f % 2 == 0)	// 짝수에 해당하는 데이터만 통지한다
 		.map(f -> f * 100)			// 데이터를 100배로 변환한다
 		.subscribe(System.out::println);
+
+		// 3-1. 일반적인 방법
+		// 리스트에서 Iterator를 얻는다
+		List<String> listArr = Arrays.asList("a", "b", "c");
+		Iterator<String> iterator = listArr.iterator();
+		while (iterator.hasNext()) {
+			String value = iterator.next();
+			System.out.println(value);
+		}
+
+		// 3-2. RxJava
+		// 리스트로 Flowable을 생성한다
+		List<String> listFlow = Arrays.asList("a", "b", "c");
+		Flowable<String> flowableList = Flowable.fromIterable(listFlow);
+
+		// 처리를 시작한다
+		flowableList.subscribe(System.out::println);
 
 	}
 
